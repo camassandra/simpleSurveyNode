8abd5aeb9201c1b00dbdd44ae1e1314f0392f651
camassandra <pontecamilla@yahoo.com>
2017-05-21 17:23:26 +0200

    first fields and basic page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const app = express();
+const port = '3000';
 
 const bodyParser = require('body-parser');
 
@@ -13,5 +14,17 @@ app.set('view engine', 'html');
 app.get('/', function(req, res){
 	console.log('rendering survey page')
 	//res.sendFile('surveypage');
-	res.sendFile(__dirname+'/views/index.html');
+	res.sendFile(__dirname+'/views/surveypage.html');
+});
+
+
+// app.post('/surveyhandler', function(req, res){
+// console.log('posting survey results')
+// res.redirect(__dirname+'/views/success.html');
+
+// });
+
+
+app.listen(port, function(){
+	console.log('listening on '+port+' has started');
 })
